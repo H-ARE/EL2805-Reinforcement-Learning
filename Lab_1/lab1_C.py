f42be4967d085a9b5ca276d0092b83e7c5516996
Harald Stiff <hstiff@kth.se>
2018-11-13 19:22:29 +0100

    Added minotour state dynamics

--- Lab_1/lab1_C.py
+++ Lab_1/lab1_C.py
@@ -3,80 +3,106 @@ from random import randint
 
 class Maze:
 
-    def __init__ (self, S, reward, A):
-        self.S = S
+    def __init__ (self, reward, A):
+        self.S = self.get_states()
         self.r = reward
         self.A = A
         self.s = [0,0]
-        self.a = self.A[randint(0,3)]
+        self.sb = [4,4]
 
-    def valid_move(self):
+    def valid_move(self, a):
         valid = True
-        if self.a == 'up' and self.s[0] == 0: # Upper Maze Wall
+        if a == 'up' and self.s[0] == 0: # Upper Maze Wall
             valid = False
-        elif self.a == 'down' and self.s[0] == 4: # Lower Maze Wall
+        elif a == 'down' and self.s[0] == 4: # Lower Maze Wall
             valid = False
-        elif self.a == 'left' and self.s[1] == 0: # Western Maze Wall
+        elif a == 'left' and self.s[1] == 0: # Western Maze Wall
             valid = False
-        elif self.a == 'right' and self.s[1] == 5: # Eastern Maze Wall
+        elif a == 'right' and self.s[1] == 5: # Eastern Maze Wall
             valid = False
     #Vertical Wall @ (0,1)   
-        elif self.a == 'right' and self.s[1] == 1 and self.s[0] in [0,1,2]:   
+        elif a == 'right' and self.s[1] == 1 and self.s[0] in [0,1,2]:   
             valid = False
-        elif self.a == 'left' and self.s[1] == 2 and self.s[0] in [0,1,2]:
+        elif a == 'left' and self.s[1] == 2 and self.s[0] in [0,1,2]:
             valid = False
     # Horizontal Wall @ (3,1)    
-        elif self.a == 'down' and self.s[0] == 3 and self.s[1] in [1,2,3,4]:
+        elif a == 'down' and self.s[0] == 3 and self.s[1] in [1,2,3,4]:
             valid = False
-        elif self.a == 'up' and self.s[0] == 4 and self.s[1] in [1,2,3,4]:
+        elif a == 'up' and self.s[0] == 4 and self.s[1] in [1,2,3,4]:
             valid = False
     # Horizontal Wall @ (1,4)       
-        elif self.a == 'down' and self.s[0] == 1 and self.s[0] in [4,5]:
+        elif a == 'down' and self.s[0] == 1 and self.s[0] in [4,5]:
             valid = False
-        elif self.a == 'up' and self.s[0] == 2 and self.s[0] in [4,5]:
+        elif a == 'up' and self.s[0] == 2 and self.s[0] in [4,5]:
             valid = False   
     # Vertical Wall @(1,3) 
-        elif self.a == 'right' and self.s[1] == 3 and self.s[0] in [1,2]:	
+        elif a == 'right' and self.s[1] == 3 and self.s[0] in [1,2]:	
             valid = False
-        elif self.a == 'left' and self.s[1] == 4 and self.s[0] in [1,2]:
+        elif a == 'left' and self.s[1] == 4 and self.s[0] in [1,2]:
             valid = False
     # Vertical Wall @(3,4)        
-        elif self.a == 'right' and self.s[1] == 3 and self.s[0] == 4:
+        elif a == 'right' and self.s[1] == 3 and self.s[0] == 4:
             valid = False
-        elif self.a == 'left' and self.s[1] == 4 and self.s[0] == 4:
+        elif a == 'left' and self.s[1] == 4 and self.s[0] == 4:
             valid = False
         return valid
 
-    def update_state(self):
-        if Maze.valid_move(self):
-            if self.a == 'up':
-                self.s[0] -= 1
-            elif self.a == 'down':
-                self.s[0] += 1
-            elif self.a == 'right':
-                self.s[1] += 1
-            elif self.a == 'left':
-                self.s[1] -= 1
-            elif self.a == 'still':
-                pass
+
+    def get_minotaor_action(self):
+        a = np.random.choice(['up', 'down', 'left', 'right'], 1, p = [0.25, 0.25, 0.25, 0.25])[0]
+
+        if a == 'up' and self.sb[0] == 0: # Upper Maze Wall
+            a = self.get_minotaor_action()
+        if a == 'down' and self.sb[0] == 4: # Lower Maze Wall
+            a = self.get_minotaor_action()
+        if a == 'left' and self.sb[1] == 0: # Western Maze Wall
+            a = self.get_minotaor_action()
+        if a == 'right' and self.sb[1] == 5: # Eastern Maze Wall
+            a = self.get_minotaor_action()
+        return a
+
+
+    def update_state(self, a, minotaor = False):
+        if not minotaor:
+            if self.valid_move(a):
+                state = self.s
             else:
-                pass
+                a = 'still'
+        else:
+            state = self.sb
+            a = self.get_minotaor_action()
+        if a == 'up':
+            state[0] -= 1
+        elif a == 'down':
+            state[0] += 1
+        elif a == 'right':
+            state[1] += 1
+        elif a == 'left':
+            state[1] -= 1
+        elif a == 'still':
+            pass
+        else:
+            pass
     def bellman(self):
         u = 0
         #if self.T > 1:
-          #Nogonting rekursivt.  
+          #Nogonting rekursivt.
+    def get_states(self):
+        rows = 5
+        columns = 6
+        S = []
+        for i in range(rows):
+            for j in range(columns):
+                S.append([i,j])
+        self.S = S
+    def get_actions(self):
+        pass
 
-def create_states():
-	rows = 5
-	columns = 6
-	S = []
-	for i in range(rows):
-		for j in range(columns):
-			S.append([i,j])
-	return S
 
-S = create_states()
+
+
+
 reward = {'dead': -100, 'end': 100, 'step': -1}
 A = ['up', 'down', 'left', 'right', 'still']
 
-s = Maze(S,reward,A)
+s = Maze(reward,A)
